d39d4ae319fca87d70fcd72f2480ced618354d64
lmiguelmh <lmamani@LMAMANI.reniecperu.net>
2016-03-14 16:28:52 -0500

    maven nar latest version and small fixes

--- clion-executable-install.bat
+++ clion-executable-install.bat
@@ -1,3 +1,4 @@
 cd clion-executable
 call mvn clean install
 pause
+cd..
---
+++ clion-executable/target/classes/archetype-resources/src/main/include/main.h
@@ -0,0 +1,5 @@
+/* 
+This is the directory where all headers will be (src/include)
+For example, it will be easy to migrate the project to a static library 
+It is recommend to use namespaces as physical directory levels (much like Java does)
+*/
