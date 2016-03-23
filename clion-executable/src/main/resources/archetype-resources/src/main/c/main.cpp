e4062b2a8668f564a5e4a670708b015b8c43ec20
lmiguelmh <lmamani@LMAMANI.reniecperu.net>
2016-03-23 09:35:38 -0500

    google test and log4z initialization

--- clion-executable/src/main/resources/archetype-resources/src/main/c/main.cpp
+++ clion-executable/src/main/resources/archetype-resources/src/main/c/main.cpp
@@ -15,7 +15,7 @@ int main() {
 	/* 
 	Log4z initialize
 	ILog4zManager::getRef().setLoggerLevel(LOG4Z_MAIN_LOGGER_ID, LOG_LEVEL_TRACE);
-    ILog4zManager::getRef().setLoggerPath(LOG4Z_MAIN_LOGGER_ID, ".");
+    ILog4zManager::getRef().setLoggerPath(LOG4Z_MAIN_LOGGER_ID, "./");
     ILog4zManager::getRef().setLoggerName(LOG4Z_MAIN_LOGGER_ID, "${artifactId}-log"); //write file
     ILog4zManager::getRef().setLoggerFileLine(LOG4Z_MAIN_LOGGER_ID, false);
     ILog4zManager::getRef().setLoggerDisplay(LOG4Z_MAIN_LOGGER_ID, false); //display in stdout?
--- clion-executable/src/main/resources/archetype-resources/src/test/c/main-test.cpp
+++ clion-executable/src/main/resources/archetype-resources/src/test/c/main-test.cpp
@@ -9,16 +9,30 @@ It is recommend to use namespaces as physical directory levels (much like Java d
 \#include <gtest/gtest.h>
 */
 
+/* Log4z
+\#include <log4z/log4z.h>
+using namespace zsummer::log4z;
+*/
+
 \#include <iostream>
 using namespace std;
 
 int main(int argc, char **argv) {
 	
+	/* Log4z: initialize logger
+	ILog4zManager::getRef().setLoggerName(LOG4Z_MAIN_LOGGER_ID, "${artifactId}-test");
+    ILog4zManager::getRef().setLoggerPath(LOG4Z_MAIN_LOGGER_ID, "./");
+    ILog4zManager::getRef().setLoggerLevel(LOG4Z_MAIN_LOGGER_ID, LOG_LEVEL_TRACE);
+    ILog4zManager::getRef().setLoggerFileLine(LOG4Z_MAIN_LOGGER_ID, true);
+    ILog4zManager::getRef().setLoggerDisplay(LOG4Z_MAIN_LOGGER_ID, true);
+    ILog4zManager::getRef().start();
+	*/
+	
 	/* Google test: initialize and run *all* the tests
 	testing::InitGoogleTest(&argc, argv);
 	RUN_ALL_TESTS();
 	*/
-	
+			
 	cout << "${artifactId}-test: Hello world from Maven NAR Project and with CLion IDE!" << endl;
 	cout << "${artifactId}-test: Now you should open the terminal set your compiler path and type: mvn test or mvn install" << endl;
     return 0;
